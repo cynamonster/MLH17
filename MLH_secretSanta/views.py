79186b69c36e78b90deb06fb1d42fc97cd45851f
Jesse Anderson <sockeyes51@gmail.com>
2017-12-02 13:26:03 -0500

    /MLH_secretSanta/ URI is working

--- MLH_secretSanta/views.py
+++ MLH_secretSanta/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-from django.http import response
+from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("Hello World!")
