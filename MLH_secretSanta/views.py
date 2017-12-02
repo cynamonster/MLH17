0ec00b2aabdd6ea3b85c6871d5cd62ced0172d66
Jesse Anderson <sockeyes51@gmail.com>
2017-12-02 12:54:07 -0500

    urls.py file

--- MLH_secretSanta/views.py
+++ MLH_secretSanta/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.http import response
+
+def index(request):
+    return HttpResponse("Hello World!")
--- Secret_Santa/urls.py
+++ Secret_Santa/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from . import views
 
 urlpatterns = [
+    url(r'^MLH_secretSanta/', include('MLH_secretSanta.urls')),
     url(r'^admin/', admin.site.urls),
 ]
